cfbe74d05f9c6db72ecfc655fa6fcd200df807d0
RenFrost <diidnugroho@gmail.com>
2024-08-13 15:17:54 +0700

    test

---
+++ script.js
@@ -0,0 +1,27 @@
+const sickness = [
+    {
+        name: "Covid",
+        symptoms: [
+            "Fever",
+            "Cough",
+            "Shortness of breath",
+            "Sore throat",
+            "Congestion",
+            "Loss of taste or smell",
+            "Fatigue",
+            "Muscle or body aches"
+        ]
+    },
+    {
+        name: "Common Flu",
+        symptoms: [
+            "Fever",
+            "Cough",
+            "Sore throat",
+            "Runny or stuffy nose",
+            "Fatigue",
+            "Headaches",
+            "Muscle or body aches"
+        ]
+    }
+]
