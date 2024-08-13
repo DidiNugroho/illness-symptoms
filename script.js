623421d3ae2fba2fca1086e00704c5978167e2eb
RenFrost <diidnugroho@gmail.com>
2024-08-13 18:47:12 +0700

    masukin data common illness

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sickness = [
+const illness = [
     {
         name: "Covid",
         symptoms: [
@@ -13,15 +13,68 @@ const sickness = [
         ]
     },
     {
-        name: "Common Flu",
+        name: "Flu",
         symptoms: [
             "Fever",
-            "Cough",
+            "Aches and pains",
+            "Dry Cough",
             "Sore throat",
-            "Runny or stuffy nose",
-            "Fatigue",
-            "Headaches",
-            "Muscle or body aches"
+            "Headache",
+            "Loss of appetite"
         ]
-    }
+    },
+    {
+        name: "Common cold",
+        symptoms: [
+            "Blocked or runny nose",
+            "Sore throat",
+            "Headaches",
+            "Cough",
+            "Sneezing",
+            "pressure in your ears and face",
+            "Loss of taste or smell"
+        ]
+    },
+    {
+        name: "Diarrhoea and Vomiting",
+        symptoms: [
+           "Low-grade fever",
+           "Stomach pain",
+           "Nausea",
+           "Watery, usually nonbloody diarrhea",
+           "Occasional muscle aches or headache"
+        ]
+    },
+    {
+        name: "Dehydration",
+        symptoms: [
+           "Feeling thirsty",
+           "dark yellow and strong-smelling pee",
+           "feeling dizzy or lightheaded",
+           "feeling tired",
+           "dry mouth, lips and eyes",
+           "peeing little, and fewer than 4 times a day"
+        ]
+    },
+    {
+        name: "Sinusitis",
+        symptoms: [
+           "Pain, swelling and tenderness around your cheeks, eyes, or forehead",
+           "a blocked nose",
+           "a reduced sense of smell",
+           "a sinus headache",
+           "green or yellow mucus from your nose",
+           "fever",
+           "tootache",
+           "bad breath"
+        ]
+    },
+    {
+        name: "Earache",
+        symptoms: [
+           "severe pain (caused by the pressure of mucus on the eardrum)",
+           "a high temperature - 38 degrees Celsius or above",
+           "slight hearing loss"
+        ]
+    },
 ]
